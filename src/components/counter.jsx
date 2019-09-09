e5075af97d8d621d3319f363982a080a41329924
tengkusyafiq <tengku.nohd.syafiq@gmail.com>
2019-09-09 16:12:06 +0800

    conditional render (if then render)

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,34 +6,23 @@ class Counter extends Component {
     tags: ["tag1", "tag2", "tag3"]
   };
 
-  render() {
+  renderTags() {
+    // if no tag, use below
+    if (this.state.tags.length === 0) return <p>the are no tags!</p>;
+
+    //else use below
+    // this will render a list from tags properties
     return (
-      <div>
-        {/* use class variable as classname */}
-        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        {/* call method formatCount*/}
-        <button className="btn btn-secondary btn-sml">Increment</button>
-        {/* this will render a list from tags properties */}
-        <ul>
-          {this.state.tags.map(tag => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
-      </div>
+      <ul>
+        {this.state.tags.map(tag => (
+          <li key={tag}> {tag}</li>
+        ))}
+      </ul>
     );
   }
 
-  getBadgeClasses() {
-    let classes = "badge m-2 badge-"; //make variable for classname
-    //append classes value with if else statement below
-    classes += this.state.count === 0 ? "warning" : "primary";
-    return classes;
-  }
-
-  formatCount() {
-    const { count } = this.state; /* simplified this.state to count*/
-
-    return count === 0 ? "Zero" : count; /* return yada2 : else return count*/
+  render() {
+    return <div>{this.renderTags()}</div>;
   }
 }
 
