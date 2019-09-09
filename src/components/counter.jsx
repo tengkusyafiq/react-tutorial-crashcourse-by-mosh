e055743d7cc620892368ca2913ad4fcf2692a016
tengkusyafiq <tengku.nohd.syafiq@gmail.com>
2019-09-09 14:25:46 +0800

    use state and call properties value dynamically

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  state = {
+    count: 0
+  };
   render() {
     return (
       <React.Fragment>
-        <h1>Hello World</h1>
+        {/* this=referring to this class */}
+        {/* state.count=referring to object state, properties count */}
+        {/* line below print count value */}
+        <span>{this.state.count}</span>
         <button>Increment</button>
       </React.Fragment>
     );
